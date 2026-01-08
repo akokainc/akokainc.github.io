a6819da40e52a33eec15035df73d8651e1aea5b8
rushilshakya <rushil.shakya@gmail.com>
2026-01-08 12:37:37 +0545

    deploy to website!

---
+++ _next/static/chunks/app/page-f01a8daef3289c5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{623:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.bind(n,757))}},e=>{e.O(0,[999,619,757,441,255,358],()=>e(e.s=623)),_N_E=e.O()}]);
